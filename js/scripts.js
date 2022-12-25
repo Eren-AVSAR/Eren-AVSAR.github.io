b99571a7a159e8cc9005f151a4eff17e3c64e684
Eren Avşar <97852181+Eren-AVSAR@users.noreply.github.com>
2022-12-25 23:27:13 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,7 @@
 
 
     /* Countdown Timer - The Final Countdown */
-	$('#clock').countdown('2023/6/18 19:38:00') /* change here your "countdown to" date */
+	$('#clock').countdown('2023/18/6 19:38:00') /* change here your "countdown to" date */
 	.on('update.countdown', function(event) {
 		var format = '<span class="counter-number">%D<br><span class="timer-text">Gün</span></span><span class="counter-number">%H<br><span class="timer-text">Saat</span></span><span class="counter-number">%M<br><span class="timer-text">Dakika</span></span><span class="counter-number">%S<br><span class="timer-text">Saniye</span></span>';
 		$(this).html(event.strftime(format));
